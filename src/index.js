4052d6c7390dddfa191988f9056ed4ad8a85dd4e
LaisDeMoraes <103288918+LaisDeMoraes@users.noreply.github.com>
2022-08-12 11:18:32 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -54,8 +54,8 @@ function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp );
-  document.querySelector("#humidity").innerHTML = reponse.data.main.humidity;
-  document.querySelector("#wind").innerHTML = reponse.data.main.wind.speed;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
 }
 
 function search(event) {
