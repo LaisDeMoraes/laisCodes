1a1d9aea935fcbf3de32f6bd9af21cf42c3410ff
LaisDeMoraes <103288918+LaisDeMoraes@users.noreply.github.com>
2022-08-19 19:32:44 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -72,31 +72,23 @@ function displayForecast() {
   let days = ["Thu", "Fri", "Sat", "Sun"];
   
   let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        `
-      <div class="col-2">
-        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-        <img
-          src="http://openweathermap.org/img/wn/${
-            forecastDay.weather[0].icon
-          }@2x.png"
-          alt=""
-          width="42"
-        />
-        <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${Math.round(
-            forecastDay.temp.max
-          )}° </span>
-          <span class="weather-forecast-temperature-min"> ${Math.round(
-            forecastDay.temp.min
-          )}° </span>
-        </div>
-      </div>
-  `;
-    }
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+             ` <div class="col-2"> 
+                <div class="weather-forecast-date">
+                  Thu </div>
+                        <img
+              src="http://openweathermap.org/img/wn/01d@2x.png"
+              alt="" width="36"/>
+               <div class="weather-forecast-temperatures">
+                 <span class="weather-forecast-temperature-max">
+                   18° </span>
+                  <span class="weather-forecast-temperature-min">
+                 12°   </span>
+                </div>
+                     </div>
+                `;
   });
 
   forecastHTML = forecastHTML + `</div>`;
