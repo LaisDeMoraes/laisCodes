f4ba7c427375b87dd600a94578da215a74facac4
LaisDeMoraes <103288918+LaisDeMoraes@users.noreply.github.com>
2022-08-19 11:31:50 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -64,6 +64,7 @@ function search(event) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
+displayForecast();
 
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
