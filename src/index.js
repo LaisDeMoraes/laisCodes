d8c115bc41522ddaa7e4d24fcad71e42f37cf12f
LaisDeMoraes <103288918+LaisDeMoraes@users.noreply.github.com>
2022-08-20 20:41:41 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -128,3 +128,4 @@ function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
+
